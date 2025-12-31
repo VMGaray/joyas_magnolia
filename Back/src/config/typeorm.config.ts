9b90c875b0f8265e644ce3421cd910fcbec23fcf
VMGaray <132283775+VMGaray@users.noreply.github.com>
2025-12-31 07:31:31 -0300

    modif package.json

--- Back/src/config/typeorm.config.ts
+++ Back/src/config/typeorm.config.ts
@@ -14,6 +14,7 @@ const config = {
   password: process.env.DB_PASSWORD || 'password',
   database: process.env.DB_NAME || 'aidi_db',
   synchronize: true,
+  //dropSchema: true,
   logging: ['error'],
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/migrations/*{.ts,.js}'],
